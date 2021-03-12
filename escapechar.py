dc226b9f39a4e8323c1c359ac8996c7ade6fd6d3
xiaotinglian <lian1995226@gmail.com>
2021-03-12 02:33:23 -0500

    Initial commit

---
+++ escapechar.py
@@ -0,0 +1,7 @@
+splitstring = "this string has been\nsplit over\nseveral\nlines"
+print(splitstring)
+
+tabbedString = "1\t2\t3\t4\t5"
+print(tabbedString)
+
+print('a person said" adhvajhbd, \' e \'e...he\' sleep')
---
+++ strings.py
@@ -0,0 +1,9 @@
+print("today is fun day")
+print('python is fun')
+print("python's string are ok")
+print('wecan do this way "as" well')
+print("hello"+" world")
+greeting = "Hello"
+name= input("please enter your name ")
+print(greeting+ ' ' + name)
+print(greeting+ ' ' + name)
