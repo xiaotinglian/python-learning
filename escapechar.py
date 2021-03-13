e1906b2ad08b288ea6ea9d0dcf8d5d4ba88d19c2
xiaotinglian <lian1995226@gmail.com>
2021-03-13 17:35:41 -0500

    git remote add origin https://github.com/xiaotinglian/udemy-python.git

--- escapechar.py
+++ escapechar.py
@@ -5,3 +5,10 @@ tabbedString = "1\t2\t3\t4\t5"
 print(tabbedString)
 
 print('a person said" adhvajhbd, \' e \'e...he\' sleep')
+
+anotherString = """this is the \
+other \
+split \
+string"""
+print(anotherString)
+print('C:\\Users\\lian1\\Desktop\\linux.txt')
--- strings.py
+++ strings.py
@@ -4,6 +4,16 @@ print("python's string are ok")
 print('wecan do this way "as" well')
 print("hello"+" world")
 greeting = "Hello"
-name= input("please enter your name ")
+name= "tim"
 print(greeting+ ' ' + name)
-print(greeting+ ' ' + name)
+
+
+age = 0.6568735436546343614345634365
+print(age)
+
+print(type(greeting))
+print(type(age))
+
+age= "2 years"
+print(name + " is " + age + " years old")
+print(type(age))
